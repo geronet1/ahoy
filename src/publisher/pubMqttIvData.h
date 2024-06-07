b7cdb69bf7417daffdde728d5552a628665ee2c5
geronet1 <geronet.bachmaier@gmail.com>
2024-06-07 08:13:16 +0200

    active_Powerlimit

--- src/publisher/pubMqttIvData.h
+++ src/publisher/pubMqttIvData.h
@@ -201,7 +201,17 @@ class PubMqttIvData {
                         if (!mJson) {
                             snprintf(mSubTopic.data(), mSubTopic.size(), "%s/ch%d/%s", mIv->config->name, rec->assign[mPos].ch, fields[rec->assign[mPos].fieldId]);
                             snprintf(mVal.data(), mVal.size(), "%g", ah::round3(mIv->getValue(mPos, rec)));
+                        } else
+                        {
+                            if (FLD_ACT_ACTIVE_PWR_LIMIT == rec->assign[mPos].fieldId)
+                            {
+                                uint8_t qos = (FLD_ACT_ACTIVE_PWR_LIMIT == rec->assign[mPos].fieldId) ? QOS_2 : QOS_0;
+                                snprintf(mSubTopic.data(), mSubTopic.size(), "%s/%s", mIv->config->name, fields[rec->assign[mPos].fieldId]);
+                                snprintf(mVal.data(), mVal.size(), "%g", ah::round3(mIv->getValue(mPos, rec)));
+                                mPublish(mSubTopic.data(), mVal.data(), retained, qos);
+                            }
                         }
+
                     }
 
                     if (InverterDevInform_All == mCmd || InverterDevInform_Simple == mCmd || !mJson) {
